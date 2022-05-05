6fcfe0398ee7be74a5d7d26e53bf9f11d03e6ddf
Taylor25et <taylor25et@gmail.com>
2022-05-04 21:46:14 -0700

    updated user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,34 @@
-const { User } = require('../models');
-
-// const userController = {
-//     getUsers(req, res){
-
-//     },
-//     createUser(req, res) {
-//         User.create(req.body)
-//           .then((user) => res.json(user))
-//           .catch((err) => res.status(500).json(err));
-//       },
-// };
+const { User, Thought } = require("../models");
 
 module.exports = {
-    //get all users
-    getUser(req, res){
-        User.find().then((user) => res.json(user)).catch((err) => res.status(500).json(err));
-    },
+  //get all users
+  getUsers(req, res) {
+    User.find()
+      .then((user) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.objectId })
+      .select("-__v")
+      .populate("thoughts")
+      .populate({
+        path: "friends",
+        select: ["-__v"],
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User id not found...." })
+          : res.status(200).json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  //create users
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+//might need to pull in user module
+// const { User } = require('../../models');
+const { getUsers, createUser } = require("../../controllers/userController");
 
-const { createUser } = require("../../controllers/userController");
-
-router.route("/").get().post(createUser);
+router.route('/').get(getUsers).post(createUser);
 
 module.exports = router;
